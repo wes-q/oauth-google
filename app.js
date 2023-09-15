76ac7351497fa9c7a136268030cde13c4f516c7b
iamwesofph <piuswesley@gmail.com>
2023-09-15 15:21:57 +0800

    Finish MVP

--- app.js
+++ app.js
@@ -1,43 +1,62 @@
-require('dotenv').config();
+require("dotenv").config();
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
+//Add session support
+var session = require("express-session");
+var passport = require("passport");
+var SQLiteStore = require("connect-sqlite3")(session);
+
+var indexRouter = require("./routes/index");
+var authRouter = require("./routes/auth");
 
 var app = express();
 
-app.locals.pluralize = require('pluralize');
+app.locals.pluralize = require("pluralize");
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
+//Add session support
+app.use(
+    session({
+        secret: "x kitten",
+        resave: false,
+        saveUninitialized: false,
+        store: new SQLiteStore({ db: "sessions.db", dir: "./var/db" }),
+    })
+);
+//Authenticate the session
+app.use(passport.authenticate("session"));
+
+app.use("/", indexRouter);
+app.use("/", authRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
